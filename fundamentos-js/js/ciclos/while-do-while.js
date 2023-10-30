160c1619908124f4a5ca8e38ec0d4104d7b8bb9a
Jose Hurtado <joseolmer11@gmail.com>
2023-10-30 18:48:28 -0500

    30/10/2023

---
+++ fundamentos-js/js/ciclos/while-do-while.js
@@ -0,0 +1,24 @@
+
+//while
+
+const carros = ['Ford', 'Mazda', 'Honda', 'Toyota'];
+
+let i = 0;
+
+while(i < carros.length) {
+    console.log(carros[i]);
+    i++;
+}
+
+
+
+//Do While
+//Ejecuta el bloque interno una vez y luego evalua condición
+
+let j = 1;
+
+do {
+    console.log(carros[j]);
+    j++;
+} while(carros[j]);
+
