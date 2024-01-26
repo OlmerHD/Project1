dbc69d4470e55944799486ea32a9e7e9e8964beb
OlmerHD <146297373+OlmerHD@users.noreply.github.com>
2024-01-26 15:41:28 -0500

    ejercicio calculadora

--- FCC JS 1/Practice1.js
+++ FCC JS 1/Practice1.js
@@ -1,1 +1,17 @@
-console.log("Hola world");
+
+let numDec = 15;
+let numDecSec = 5;
+
+let resulDec = numDec % numDecSec;
+
+console.log(resulDec);
+
+let miString = 'string';
+
+console.log(miString[3]);
+
+console.log(miString.length);
+
+
+
+
