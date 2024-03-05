9141a100a850cb350e210977430e20daf6420538
OlmerHD <146297373+OlmerHD@users.noreply.github.com>
2024-03-05 17:08:19 -0500

    automatizando pruebas con cypress p.2

--- Demo_Cypress1/cypress/e2e/smoke_testbanco.cy.js
+++ Demo_Cypress1/cypress/e2e/smoke_testbanco.cy.js
@@ -1,12 +1,50 @@
 describe("Test suite 1 - conjunto de pruebas", () => {
 
-    it("Validar pagina de inicio", () => {
+    //uso de Hooks para optimizar codigo repetido
+    beforeEach(() => {
 
-        cy.visit('https://buggy.justtestit.org/')
-        cy.get('.center-block > .img-fluid').should('be.visible')
-        cy.get(':nth-child(3) > .card > .card-block > h3')
+        cy.visit('http://zero.webappsecurity.com/')
+
     })
 
 
+    //Test iniciales
+ 
+    it("Validar pagina de inicio", () => {
+
+        cy.get('.active > img').should('be.visible')
+        cy.get('.active > .custom > h4').contains('Online Banking')
+    })
+
+    it("Prueba E2E - transferencia de fondos ", () => {
+
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type('username')
+        cy.get('#user_password').type('password')
+        cy.get('.btn').click()
+        cy.get('#transfer_funds_tab > a').click()
+        cy.get('#tf_fromAccountId').select('1')
+        cy.get('#tf_toAccountId').select('5')
+        cy.get('#tf_amount').type('300')
+        cy.get('#tf_description').type('Transferencia de prueba')
+        cy.get('#btn_submit').click()
+        cy.get('#btn_submit').click()
+        cy.get('.alert').contains("You successfully submitted your transaction.")
+
+    }) 
+
+    //Only para que solo se ejecute esa prueba
+    it.only("Prueba de actualizacion del grafico", () => {
+
+        cy.get('#signin_button').click()
+        cy.get('#user_login').type('username')
+        cy.get('#user_password').type('password')
+        cy.get('.btn').click()
+        cy.get('#money_map_tab > a').click()
+        cy.get('#ext-sprite-1259').should('be.visible')
+        cy.get('#ext-sprite-1168').click()
+        cy.get('#ext-sprite-1259').should('not.be.visible')
+
+    })
 
 }) 
